8cc66de9a30d5fb0057004a87b39fc2018e2542e
thrivediraju <104252421+thrivediraju@users.noreply.github.com>
2022-06-10 21:48:58 +0530

    Add files via upload

---
+++ details_input.py
@@ -0,0 +1,14 @@
+name=input()
+empid= int(input())
+salary=float(input())
+
+# print(name, empid , salary)
+
+# print('My Name is',name,'and Empid is',empid)
+# print('Salary for the month of may is',salary)
+
+# print('My Name is %s and Empid is %d' %(name,empid))
+# print('Salary for the month of may is %.2f' %salary)
+
+print(f'My Name is {name} and Empid is {empid}')
+print(f'Salary for the month of may is {salary}')
